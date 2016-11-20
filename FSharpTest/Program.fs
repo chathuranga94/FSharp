3e0d02ef20dfe260ad2e6c25c1759f1f70f35394
chathuranga94 <chathuranga94@live.com>
2016-11-20 14:52:34 +0530

    chain functions added

--- FSharpTest/Program.fs
+++ FSharpTest/Program.fs
@@ -3,21 +3,54 @@
 open System
 
 let square x = x * x        // ALT + ENTER for F# Interactive   (use ';;')
-let isOdd x = x % 2 <> 0   
+let negative x = x * -1
+let print x = printfn "The number is: %d" x
+
+let chainFunction1 x =
+    print (negative (square 12))
+let chainFunction2 x =
+    x |> square |> negative |> print
+let compositeFunction =
+    square >> negative >> print
+
+
+let rec fact x =
+    if x < 1 then 1
+    else x * fact (x - 1)
+
+let rec even x =
+   if x = 0 then true 
+   else odd (x - 1)
+
+and odd x =
+   if x = 1 then true 
+   else even (x - 1)
+
+let isOdd x = x % 2 <> 0  
+//let read = Console.ReadLine()
 
 [<EntryPoint>]
 let main argv = 
     printfn "%d squared is: %d!" 12 (square 12)     //  sprintf
-    let read = Console.ReadLine()
-    0 // return an integer exit code
+    (print 10)
 
-(*
-open System
+    chainFunction1 12
+    chainFunction2 10
 
-let main() =
-    Console.Write("What's your name? ")
+    printfn "Recursive Functions: Factorial %d IsEven %b" (fact 5) (even 10)
+
     let name = Console.ReadLine()
     Console.Write("Hello, {0}", name)
+    0 // return an integer exit code
+
+
+(*
+        open System
+
+        let main() =
+            Console.Write("What's your name? ")
+            let name = Console.ReadLine()
+            Console.Write("Hello, {0}", name)
     
-main()
+        main()
 *)
